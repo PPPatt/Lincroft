e223c173d92e4a9241fcf154c6b1cac34c75ac16
Phillip <phillippatt@Phillips-Air.fritz.box>
2016-11-22 19:37:04 +0100

    temporary solution for updating state(resetting the whole thing instead of only what has changed)

--- app/store/reducers/projectsReducer.js
+++ app/store/reducers/projectsReducer.js
@@ -6,6 +6,8 @@ module.exports = (currentstate, action) => {
   switch(action.type) {
     case C.ADD_PROJECTS:
       return {...action.projects, ...currentstate}
+    case C.UPDATE_PROJECTS:
+      return action.projects
     default: return currentstate|| initialstate.projects
   }
 }
