0fa022577c29b5054216b9711aee9f53922f8453
Phillip <phillippatt@Phillips-Air.fritz.box>
2016-11-23 15:07:57 +0100

    implement CreateNewProject modal in Lobby

---
+++ app/view/createnewproject.js
@@ -0,0 +1,53 @@
+//TODO: resort/rename view and reusable components
+
+'use strict'
+import React, {Component} from 'react'
+import {View, Text, TextInput} from 'react-native'
+
+const firebase = require('../reusables/firebase')
+const Button = require('../reusables/button')
+
+class CreateNewProject extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      addUser: '',
+      userList: [],
+    }
+  }
+  render() {
+    return(
+      <View>
+        <Text>Title</Text>
+        <TextInput
+          onChangeText={(i)=>{this.setState({title: i})}}
+          value={this.state.title}/>
+        <Text>add User</Text>
+        <TextInput
+          onChangeText={(i)=>{this.setState({addUser: i})}}
+          onSubmitEditing={(i)=>{
+            let newUserList = this.state.userList
+            newUserList.push({id: this.state.addUser})
+            this.setState({userList: newUserList, addUser: ''})
+          }}
+          value={this.state.addUser}/>
+        <Text>{this.state.userList.map((e)=>e.id).toString()}</Text>
+        <Button onPress={()=>{
+          firebase.addProject({
+            properties: {
+              title: this.state.title,
+              members: this.state.userList,
+            },
+            someKey: 'someValue',
+          })
+          this.props.switchVisible()
+        }}>
+        commit
+        </Button>
+      </View>
+    )
+  }
+}
+
+module.exports = CreateNewProject
--- app/view/lobby.js
+++ app/view/lobby.js
@@ -5,16 +5,34 @@ import {View, Text} from 'react-native'
 const ReactRedux = require('react-redux')
 
 const ViewContainer = require('../reusables/viewcontainer')
-const ProjectList = require('../reusables/projectlist.js')
+const ProjectList = require('../reusables/projectlist')
+const Modal = require('../reusables/modal')
+const CreateNewProject = require('./createnewproject')
 
 class Lobby extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      ModalVisible_CreateNewProject: false
+    }
+  }
+  
+  switchVisible() {
+    this.setState({ModalVisible_CreateNewProject: !this.state.ModalVisible_CreateNewProject})
+  }
+
   render() {
     return(
-      <ViewContainer title='Lobby' rightButton={true} buttonTitle='add' onPressRight={()=>{this.onNavbarRightPressed()}} back={()=>this.props.navigator.pop()}>
-          <ProjectList
-            onPress={(rowData)=>{
-              this.props.navigator.push({id:'ProjectOverview', projID: rowData.id})
-            }}/>
+      <ViewContainer title='Lobby' rightButton={true} buttonTitle='add' onPressRight={()=>{this.switchVisible()}} back={()=>this.props.navigator.pop()}>
+        {this.state.ModalVisible_CreateNewProject?
+          <Modal switchVisible={()=>{this.switchVisible()}}>
+            <CreateNewProject switchVisible={()=>{this.switchVisible()}}/>
+          </Modal>:
+          null}
+        <ProjectList
+          onPress={(rowData)=>{
+            this.props.navigator.push({id:'ProjectOverview', projID: rowData.id})
+          }}/>
       </ViewContainer>
     )
   }
