245b08df764d9a67cd5e25d4fec22682935726e0
Phillip <phillippatt@Phillips-Air.fritz.box>
2016-11-30 00:34:04 +0100

    added evaluateProject() and passed it into projectlogicView to set logicPath

--- app/view/projectlogic.js
+++ app/view/projectlogic.js
@@ -6,10 +6,26 @@ const fb = require('../reusables/firebase')
 
 const ViewContainer = require('../reusables/viewcontainer')
 const ProjectLogicView = require('../reusables/projectlogicview')
+const evaluateFunction = require('../reusables/evaluatefunction')
 
 class ProjectLogic extends Component {
   _onPressRight() {
     fb.updateProject(this.props.projID, this.props.project)
+  }
+
+  evaluateProject() {
+    let iterate = (funcID = 0, tempPath = []) => {
+      let result = evaluateFunction(this.props.project.logic[funcID])
+      tempPath.push(result.opID)
+      if(typeof(result.op.action)==='number') {
+        return iterate(result.op.action, tempPath)
+      } else {return tempPath}
+    }
+    if(this.props.project.logic) {
+      return iterate()
+    } else {
+      return []
+    }
   }
 
   render() {
@@ -20,7 +36,7 @@ class ProjectLogic extends Component {
         rightButton={true}
         buttonTitle='submit'
         onPressRight={()=>{this._onPressRight()}}>
-        <ProjectLogicView projID={this.props.projID}/>
+        <ProjectLogicView projID={this.props.projID} initialPath={this.evaluateProject()}/>
       </ViewContainer>
     )
   }
